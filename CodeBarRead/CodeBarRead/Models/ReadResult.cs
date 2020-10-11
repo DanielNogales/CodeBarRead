294df6323402af7f31a9526c0f2fbb72ff2324cf
Daniel Nogales <nogales_d@yahoo.com>
2020-10-11 12:32:09 -0300

    Add project files.

---
+++ CodeBarRead/CodeBarRead/Models/ReadResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeBarRead.Models
+{
+    public class ReadResult
+    {
+        public DateTime Date { get; set; }
+        public string Format { get; set; }
+        public string Value { get; set; }
+        public string Device { get; set; }
+    }
+}
